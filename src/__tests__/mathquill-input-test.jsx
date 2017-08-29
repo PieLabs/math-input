fa2a8841962266c8566c50e6eb8703e218abdaaa
edeustace <ed.eustace@gmail.com>
2017-08-29 22:29:26 +0100

    mount to test lifecycle

--- src/__tests__/mathquill-input-test.jsx
+++ src/__tests__/mathquill-input-test.jsx
@@ -1,20 +1,19 @@
+import { mount, shallow } from 'enzyme';
+
 import MathQuillInput from '../mathquill-input.jsx';
 import React from 'react';
 import { assert } from 'sinon';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
 import { staticField } from '../../__mocks__/mathquill-mock';
 
 const handleClick = () => { };
 
 test('mounting', () => {
-  const input = shallow(<MathQuillInput
+  const input = mount(<MathQuillInput
     readOnly={false}
     latex={'1+1=2'}
     onClick={handleClick} />
   );
-
-  // console.log('staticField: ', staticField);
 
   assert.calledWith(staticField.latex, '1+1=2');
 });
